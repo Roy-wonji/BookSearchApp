53d9ac187e5ed388c020c84903b4f7b9c2a7a564
Roy <shuwj81@daum.net>
2025-07-08 13:08:13 +0900

    ➕[add]:  프로젝트 기본  셋팅

---
+++ BookSearch/BookSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookSearch
+//
+//  Created by Wonji Suh  on 7/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ BookSearch/BookSearch/Sources/Application/BookSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSearchApp.swift
+//  BookSearch
+//
+//  Created by Wonji Suh  on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BookSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
